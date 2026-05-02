3de2f112f7b77bb4777d38d1ff736f7283801932
David Lund <david.lund@knowit.se>
2026-05-02 18:15:01 +0200

    Undu 2026

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # undutmaning
+
 My feeble attempts at being a hacker
+
+# Undu 2026
+
+This was my first year as a competitor. I actually solved 2 problems, plus the give us feedback.
+
+## Successes
+
+### Feedback
+
+Not a puzzle.
+
+### The fish tank problem
+
+Pure logic with a web app to solve it.
+
+### Some sort of Dosa/Pin code problem
+
+You put in the combos and one gave you the flag.
+
+## Failed
+
+### Åtkomst Nekad
+
+On 1 problem I figured how to crack the guys password, but once I had the password, I was stuck. I should have used SSH to go further but I didn't don't remember why.
+Perhaps running out of time. It did take a while to figure out bcrypt and how to code it in C#. Fortunately when I set up the Pokemon list, according to Wikipedia, I started with first generation (about 150) as listed and not all. For years 1990 to now. I did not do more. 
+
+### Others that I made no progress
+
+*  Krypto -> Infected bios. Tried to look in a debugger but wrong one. No clue.
+*  Krypto -> UndervattenStrömmar - had some c program and a cipher text file. But I didn't have c++ enabled in VS2026. TYhat took time and then I could make it build correctly for a while.
+* Programming - there was a lab map that one was to send movements over a connection while interpreting shark movements. Could connect and recieve shark stuff, but could not send data. And never programmed anything.
+
+## My flags
+
+Feedback
+*  undut{Hoppas vi ses nästa år igen!}
+Programming => Biologisk mångfald
+*  undut{Använde du AI är du inte sigma!}
+
+undut{99fd5378b42993139ec5042b02cc30b83df786ed}
---
+++ undu2026/Misc/ÅtkomstNekad/CrackPassword/CrackPassword/CrackPasswordHarness.cs
@@ -0,0 +1,44 @@
+﻿using BCrypt.Net;
+using Xunit;
+
+namespace CrackPassword;
+
+public class CrackPasswordHarness
+{
+    // (316, 'pelle.patchberg', 'mail.cascada.ex', '$2b$12$5rudATAULHHOSLdwlDiUZOxop6szZW6tszHLPmQ91W700NnNrOrfq'),
+    private readonly string expectedPasswordHash = "$2b$12$5rudATAULHHOSLdwlDiUZOxop6szZW6tszHLPmQ91W700NnNrOrfq"; // Example hash for "password"
+    [Fact]
+    public void SpecificPasswordCrack_OK()
+    {
+        string password = "Bulbasaur1997";
+        //string passwordHash = BCrypt.HashPassword(password);
+        var isValid = BCrypt.Net.BCrypt.Verify(password, expectedPasswordHash);
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void BruteForcePasswordCrack()
+    {
+        var pokemonList = ReadLinesFromFile("PokemonGen1.txt");
+        //pokemonList.Sort();
+        var years = Enumerable.Range(1990, 37).Select(y => y.ToString()).ToList(); // Example range of years
+        foreach (var pokemon in pokemonList)
+        {
+            foreach (var year in years)
+            {
+                string password = $"{pokemon}{year}";
+                var isValid = BCrypt.Net.BCrypt.Verify(password, expectedPasswordHash);
+                Assert.False(isValid, $"Password found: {password}"); // Fails when Password found
+            }
+        }
+        Assert.Fail("Not gen 1 Pokemon");
+    }
+
+    public string[] ReadLinesFromFile(string filename)
+    {
+        string baseDir = AppContext.BaseDirectory;
+        string filePath = $"{baseDir}\\Data\\{filename}";
+        string[] orbits = File.ReadAllLines(filePath);
+        return orbits;
+    }
+}
